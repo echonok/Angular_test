c6ed63e531fb678d77dcd3c9dbebed1312456b51
Nick Elovsky <echonok@gmail.com>
2018-12-06 09:35:15 +0300

    add files

---
+++ app/main.ts
@@ -0,0 +1,5 @@
+import { platformBrowserDinamic } from '@angular/platform-browser-dynamic';
+
+import { AppModule } from './app.module';
+const platform = platformBrowserDinamic();
+platform.bootstrapModule(AppModule);
